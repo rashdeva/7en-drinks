82c3664310485ef47911d37621983987a3f67f7d
Rashid Shamsudinov <rash2x@gmail.com>
2025-06-15 18:31:44 +0700

    fix: build

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,9 +7,9 @@ import { ConfigModule } from './config.module';
 import { AuthModule } from './auth/auth.module';
 import { NotificationModule } from './notifications/notification.module';
 import { QuestModule } from './quests/quest.module';
-import { ComboModule } from './combo/combo.module';
-import { LeaderboardModule } from './leaderboard/leaderboard.module';
-import { PaymentsModule } from './payments/payments.module';
+// import { ComboModule } from './combo/combo.module';
+// import { LeaderboardModule } from './leaderboard/leaderboard.module';
+// import { PaymentsModule } from './payments/payments.module';
 
 @Module({
   imports: [
@@ -19,9 +19,9 @@ import { PaymentsModule } from './payments/payments.module';
     AuthModule,
     NotificationModule,
     QuestModule,
-    ComboModule,
-    LeaderboardModule,
-    PaymentsModule,
+    // ComboModule,
+    // LeaderboardModule,
+    // PaymentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
