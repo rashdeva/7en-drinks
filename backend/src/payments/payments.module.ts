2bf351a8926dc4243bd69691b1d0efa253bb3040
Rashid Shamsudinov <rash2x@gmail.com>
2025-06-15 19:36:43 +0700

    fix: return it all

--- backend/src/payments/payments.module.ts
+++ backend/src/payments/payments.module.ts
@@ -1,29 +1,29 @@
-// import { Module } from '@nestjs/common';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { PaymentsService } from './payments.service';
-// import { PaymentsController } from './payments.controller';
-// import { Payment, PaymentSchema } from './entities/payment.entity';
-// import { ConfigModule } from '@nestjs/config';
-// import { User, UserSchema } from '../users/entities/user.entity';
-// import { TonProviderService } from './ton-provider.service';
-// import {
-//   Transaction,
-//   TransactionSchema,
-// } from 'src/users/entities/transaction.entity';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PaymentsService } from './payments.service';
+import { PaymentsController } from './payments.controller';
+import { Payment, PaymentSchema } from './entities/payment.entity';
+import { ConfigModule } from '@nestjs/config';
+import { User, UserSchema } from '../users/entities/user.entity';
+import { TonProviderService } from './ton-provider.service';
+import {
+  Transaction,
+  TransactionSchema,
+} from 'src/users/entities/transaction.entity';
 
-// @Module({
-//   imports: [
-//     MongooseModule.forFeature([
-//       { name: Payment.name, schema: PaymentSchema },
-//       { name: User.name, schema: UserSchema },
-//     ]),
-//     ConfigModule,
-//     MongooseModule.forFeature([
-//       { name: Transaction.name, schema: TransactionSchema },
-//     ]),
-//   ],
-//   controllers: [PaymentsController],
-//   providers: [PaymentsService, TonProviderService],
-//   exports: [PaymentsService],
-// })
-// export class PaymentsModule {}
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Payment.name, schema: PaymentSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
+    ConfigModule,
+    MongooseModule.forFeature([
+      { name: Transaction.name, schema: TransactionSchema },
+    ]),
+  ],
+  controllers: [PaymentsController],
+  providers: [PaymentsService, TonProviderService],
+  exports: [PaymentsService],
+})
+export class PaymentsModule {}
